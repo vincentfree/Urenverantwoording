be67a14607ebbcea5863f1bb013fe4bf1466518b
Vincent Free <vinsterdamus@gmail.com>
2016-10-04 15:49:39 +0200

    Update UrenverantwoordingApplication.java
    test if this works

--- src/main/java/nl/atos/ps/noord/testapp/urenverantwoording/UrenverantwoordingApplication.java
+++ src/main/java/nl/atos/ps/noord/testapp/urenverantwoording/UrenverantwoordingApplication.java
@@ -9,4 +9,11 @@ public class UrenverantwoordingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UrenverantwoordingApplication.class, args);
 	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+  		if (!registry.hasMappingForPattern("/templates/**")) {
+     			registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/templates/");
+  		}
+	}
 }
