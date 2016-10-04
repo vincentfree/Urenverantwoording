f47224f12af84688c6fba408e96b2693d9e4ca46
vincentfree <neromy_1@msn.com>
2016-10-04 16:14:39 +0200

    changed bootstrap css and js link in html

--- src/main/java/nl/atos/ps/noord/testapp/urenverantwoording/UrenverantwoordingApplication.java
+++ src/main/java/nl/atos/ps/noord/testapp/urenverantwoording/UrenverantwoordingApplication.java
@@ -2,18 +2,12 @@ package nl.atos.ps.noord.testapp.urenverantwoording;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication
 public class UrenverantwoordingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrenverantwoordingApplication.class, args);
-	}
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-  		if (!registry.hasMappingForPattern("/templates/**")) {
-     			registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/templates/");
-  		}
 	}
 }
